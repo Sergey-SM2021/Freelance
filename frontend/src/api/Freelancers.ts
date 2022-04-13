d9ac60836450653b5b63183c90eea7f0c1288424
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-04-13 22:13:19 +0300

    typing Freelancers api

--- frontend/src/api/Freelancers.ts
+++ frontend/src/api/Freelancers.ts
@@ -10,51 +10,53 @@ class Freelancers {
     constructor(baseURL: string) {
         this.baseURL = Axios.create({ baseURL })
     }
-    getFreelancers = async () => {
-        try {
-            const freelancers = (await this.baseURL.get<Array<TFreelancerApi>>("")).data
-            return this.getPreviewFreelancers(freelancers)
-        } catch (error) {
-            throw "Freelancers не были получены"
+    getFreelancers: () => Promise<Array<TFreelancer>>
+        = async () => {
+            try {
+                const freelancers = (await this.baseURL.get<Array<TFreelancerApi>>("")).data
+                return this.getPreviewFreelancers(freelancers)
+            } catch (error) {
+                throw "Freelancers не были получены"
+            }
         }
-    }
 
-    getPreviewFreelancers = (freelancers: Array<TFreelancerApi>) => {
-        try {
-            const previewFreeelancers: Array<TFreelancer> = freelancers.map(freelancer =>
-            ({
-                header: {
-                    name: freelancer.name,
-                    lastName: freelancer.lastname,
-                    ava: freelancer.ava,
-                    specialization: freelancer.specialization
-                },
-                about: {
-                    description: freelancer.description,
-                    expiriens: freelancer.expiriens,
-                    price: freelancer.price,
-                    stack: freelancer.stack,
-                    paymentMethod: freelancer.paymentmethod,
-                },
-                portfolio: [Project1, Project2, Project3],
-                id: freelancer.id,
-                reviews: freelancer.review,
-                likes: freelancer.likes,
-                dislike: freelancer.dislike,
-                workHistory: freelancer.workhistory
-            })
-            )
-            return previewFreeelancers
-        } catch (error) {
-            throw 'Не удалось получить Preview freelancer'
+    getPreviewFreelancers: (freelancers: Array<TFreelancerApi>) => Array<TFreelancer>
+        = (freelancers) => {
+            try {
+                const previewFreeelancers: Array<TFreelancer> = freelancers.map(freelancer =>
+                ({
+                    header: {
+                        name: freelancer.name,
+                        lastName: freelancer.lastname,
+                        ava: freelancer.ava,
+                        specialization: freelancer.specialization
+                    },
+                    about: {
+                        description: freelancer.description,
+                        expiriens: freelancer.expiriens,
+                        price: freelancer.price,
+                        stack: freelancer.stack,
+                        paymentMethod: freelancer.paymentmethod,
+                    },
+                    portfolio: [Project1, Project2, Project3],
+                    id: freelancer.id,
+                    reviews: freelancer.review,
+                    likes: freelancer.likes,
+                    dislike: freelancer.dislike,
+                    workHistory: freelancer.workhistory
+                })
+                )
+                return previewFreeelancers
+            } catch (error) {
+                throw 'Не удалось получить Preview freelancer'
+            }
         }
-    }
 
-    findFreelancersByName = async (name: string) => {
-        const freelancers = await (await this.baseURL.get(`find?name=${name}`)).data
-        console.log(freelancers)
-        return this.getPreviewFreelancers(freelancers)
-    }
+    findFreelancersByName: (name: string) => Promise<Array<TFreelancer>>
+        = async (name) => {
+            const freelancers = await (await this.baseURL.get<Array<TFreelancerApi>>(`find?name=${name}`)).data
+            return (this.getPreviewFreelancers(freelancers))
+        }
 }
 
 export default new Freelancers("http://localhost:8900/freelancers/")
