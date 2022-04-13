1e310c04c645f0dcad1978f2de7c2f4270ed74ee
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-04-13 22:41:03 +0300

    Typed User api

--- frontend/src/api/User.ts
+++ frontend/src/api/User.ts
@@ -4,15 +4,19 @@ import { TUserApi } from '../types/IUser'
 
 class User {
     baseUrl
+
     constructor(url: string) {
         this.baseUrl = axios.create({ baseURL: url })
     }
-    async getUser(mail: string, password: string) {
-        const user = await (await this.baseUrl.get<TUserApi>(`?mail=${mail}&password=${password}`)).data
-        return user
-    }
+
+    getUser: (mail: string, password: string) => Promise<TUserApi>
+        = async (mail, password) => {
+            const user = await (await this.baseUrl.get<TUserApi>(`?mail=${mail}&password=${password}`)).data
+            return user
+        }
+
     createUser = async (mail: string, password: string, type: string) => {
-        await this.baseUrl.post("",{mail,password,type})
+        await this.baseUrl.post("", { mail, password, type })
     }
 }
 
